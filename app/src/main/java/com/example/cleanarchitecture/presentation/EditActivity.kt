28461bf9d52a752f1453db930776025f862794d4
User <akimov1712@mail.ru>
2023-05-03 14:35:40 +0300

    add fragment to MainActivity.kt

--- app/src/main/java/com/example/cleanarchitecture/presentation/EditActivity.kt
+++ app/src/main/java/com/example/cleanarchitecture/presentation/EditActivity.kt
@@ -8,13 +8,6 @@ import com.example.cleanarchitecture.R
 import com.example.cleanarchitecture.domain.ShopItem
 
 class EditActivity : AppCompatActivity() {
-
-//    private lateinit var viewModel: EditViewModel
-//
-//    private lateinit var etName: EditText
-//    private lateinit var etCount: EditText
-//    private lateinit var buttonSend: Button
-
     private var screenMode = MODE_UNKNOWN
     private var shopItemId = ShopItem.UNDEFINED_ID
 
@@ -22,82 +15,22 @@ class EditActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit)
         parseIntent()
-//        viewModel = ViewModelProvider(this)[EditViewModel::class.java]
-//        initViews()
-//        AddTextChangeListeners()
-        launchRightMode()
-//        observeViewModel()
+        if (savedInstanceState == null){
+            launchRightMode()
+        }
     }
-//
-//    private fun observeViewModel() {
-//        viewModel.errorInputName.observe(this) {
-//            val message = if (it) getString(R.string.error_input_name) else null
-//            etName.error = message
-//        }
-//        viewModel.errorInputCount.observe(this) {
-//            val message = if (it) getString(R.string.error_input_count) else null
-//            etCount.error = message
-//        }
-//        viewModel.shouldCloseScreen.observe(this) {
-//            finish()
-//        }
-//    }
-//
+
     private fun launchRightMode() {
     val fragment = when (screenMode) {
             MODE_EDIT -> EditFragment.newInstanceModeEdit(shopItemId)
             MODE_ADD -> EditFragment.newInstanceModeAdd()
             else -> throw java.lang.RuntimeException("Unknown screen mode: $screenMode")
     }
-    supportFragmentManager.beginTransaction().add(R.id.edit_container, fragment).commit()
+    supportFragmentManager.beginTransaction()
+        .replace(R.id.edit_container, fragment)
+        .commit()
 }
-//
-//    private fun AddTextChangeListeners() {
-//        etName.addTextChangedListener(object : TextWatcher {
-//            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-//
-//            }
-//
-//            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-//                viewModel.resetErrorInputName()
-//            }
-//
-//            override fun afterTextChanged(s: Editable?) {
-//
-//            }
-//        })
-//        etCount.addTextChangedListener(object : TextWatcher {
-//            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-//
-//            }
-//
-//            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-//                viewModel.resetErrorInputCount()
-//            }
-//
-//            override fun afterTextChanged(s: Editable?) {
-//
-//            }
-//        })
-//    }
-//
-//    private fun launchEditMode() {
-//        viewModel.getShopItem(shopItemId)
-//        viewModel.shopItem.observe(this) {
-//            etName.setText(it.name)
-//            etCount.setText(it.count.toString())
-//        }
-//        buttonSend.setOnClickListener {
-//            viewModel.editShopItem(etName.text?.toString(), etCount.text?.toString())
-//        }
-//    }
-//
-//    private fun launchAddMode() {
-//        buttonSend.setOnClickListener {
-//            viewModel.insertShopItem(etName.text?.toString(), etCount.text?.toString())
-//        }
-//    }
-//
+
     private fun parseIntent() {
         if (!intent.hasExtra(SCREEN_MODE)) {
             throw java.lang.RuntimeException("Параметр screen mode не передан")
@@ -114,12 +47,6 @@ class EditActivity : AppCompatActivity() {
             shopItemId = intent.getIntExtra(SHOP_ITEM_ID, ShopItem.UNDEFINED_ID)
         }
     }
-//
-//    private fun initViews() {
-//        etName = findViewById(R.id.editTextName)
-//        etCount = findViewById(R.id.editTextCount)
-//        buttonSend = findViewById(R.id.button)
-//    }
 
     companion object {
         private const val SCREEN_MODE = "extra_mode"
--- app/src/main/java/com/example/cleanarchitecture/presentation/MainActivity.kt
+++ app/src/main/java/com/example/cleanarchitecture/presentation/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.example.cleanarchitecture.presentation
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentContainerView
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
@@ -14,20 +15,35 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
     private lateinit var shopListAdapter: ShopListAdapter
+    private var shopItemContainer: FragmentContainerView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        shopItemContainer = findViewById(R.id.fragment_container_main_activity)
         setupRecyclerView()
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         viewModel.shopList.observe(this) {
             shopListAdapter.submitList(it)
         }
         val buttonAddItem = findViewById<Button>(R.id.floatingActionButton)
-        buttonAddItem.setOnClickListener{
-            val intent = EditActivity.newIntentAddItem(this)
-            startActivity(intent)
+        buttonAddItem.setOnClickListener {
+            if (shopItemContainer == null){
+                val intent = EditActivity.newIntentAddItem(this)
+                startActivity(intent)
+            } else {
+                setupFragment(EditFragment.newInstanceModeAdd())
+            }
         }
+    }
+
+    private fun setupFragment(fragment: Fragment) {
+        supportFragmentManager.popBackStack()
+        supportFragmentManager
+            .beginTransaction()
+            .addToBackStack(null)
+            .replace(R.id.fragment_container_main_activity, fragment)
+            .commit()
     }
 
     private fun setupRecyclerView() {
@@ -73,8 +89,13 @@ class MainActivity : AppCompatActivity() {
 
     private fun setupClickListener() {
         shopListAdapter.shopItemClickListener = {
-            val intent = EditActivity.newIntentEditItem(this,it.id)
-            startActivity(intent)
+            if (shopItemContainer == null){
+                val intent = EditActivity.newIntentEditItem(this,it.id)
+                startActivity(intent)
+            } else {
+                setupFragment(EditFragment.newInstanceModeEdit(it.id))
+            }
+
         }
     }
 
